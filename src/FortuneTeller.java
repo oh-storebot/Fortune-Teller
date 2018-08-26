ce8a96dcb28305fd504be9b178262e6cbc348731
oh-storebot <eric@negron.nu>
2018-08-26 19:00:00 -0400

    version 1.0 of fortune teller

---
+++ src/FortuneTeller.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class FortuneTeller {
+
+	public static void main(String[] args) {
+	
+		Questions Teller = new Questions();
+		Teller.firstNameQuestion();
+		Teller.lastNameQuestion();
+		Teller.ageQuestion();
+		Teller.birthMonthQuestion();
+		Teller.favoriteColorQuestion();
+		Teller.numberOfSiblingsQuestion();
+		Teller.FortuneOutput();
+	}
+
+}
